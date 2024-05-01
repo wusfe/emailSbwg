4bb13a3fdfe37de9eade76d44a9ee295a5b18e05
wujie <wujie@antiy.cn>
2024-05-01 10:20:58 +0800

    指定日期

--- index.js
+++ index.js
@@ -1,33 +1,43 @@
 const schedule = require("node-schedule");
-const nodemailer = require('nodemailer');
-const config = require('./config');
-const request = require('request');
-let webStatusCode = '';
-let webStatusMsg = '';
+const nodemailer = require("nodemailer");
+const config = require("./config");
+const request = require("request");
+let webStatusCode = "";
+let webStatusMsg = "";
 
+let j = schedule.scheduleJob("30 * * * * *", function (a, b, c) {
+  console.log(a, b, c);
 
-let j = schedule.scheduleJob('30 * * * * *', function(a,b,c){
-    console.log(a, b, c)
+  request(
+    {
+      url: "https://pw.hbww.org.cn/api/ticket/calendar?p=w",
+      method: "get",
+      json: true,
+      headers: {
+        "content-type": "application/json",
+      },
+    },
+    (error, response, body) => {
+      // console.log(JSON.stringify(response))
+      let subject = "";
+      let replyStr = "";
+      let shouldEmail = false;
+      if (response.statusCode === 200) {
+        if (response.body.status === 1) {
+          // console.log(response.body.data)
 
-    request({
-        url: 'https://pw.hbww.org.cn/api/ticket/calendar?p=w',
-        method:"get",
-        json: true,
-        headers: {
-            "content-type": "application/json",
-        }
-    },(error, response, body)=>{
-        // console.log(JSON.stringify(response))
-       let subject = ''
-       let replyStr = ''
-       if(response.statusCode === 200){
-        if(response.body.status === 1){
-            // console.log(response.body.data)
-    
-            const dataArr = ['2024-05-01','2024-05-02','2024-05-03','2024-05-04','2024-05-05']
-            const data = response.body.data.yy_date.filter(item => dataArr.includes(item.t_date))
-            subject = '五一期间 省博物馆 票计：'
-             replyStr = `
+          const dataArr = [
+            "2024-05-01",
+            "2024-05-02",
+            "2024-05-03",
+            "2024-05-04",
+            "2024-05-05",
+          ];
+          const data = response.body.data.yy_date.filter((item) =>
+            dataArr.includes(item.t_date)
+          );
+          subject = "五一期间 省博物馆 票计：";
+          replyStr = `
             <table border=1>
                 <thead>
                     <th>${dataArr[0]}</th>
@@ -50,45 +60,51 @@ let j = schedule.scheduleJob('30 * * * * *', function(a,b,c){
                 </tbody>
             </table>
             
-            `
-    
-            
-          
-    
-        }else {
-            subject = '统计失败'
+            `;
 
-            replyStr = `response.body.status: ${response.body.status}`
+          shouldEmail = response.body.data.yy_date
+            .filter((item) =>
+              ["2024-05-03", "2024-05-04"].includes(item.t_date)
+            )
+            .some((t) => t.tp_last_stock_sum > 0);
+
+          // console.log()
+
+          // return
+          // if( response.body.data.yy_date.filter(item => ['2024-05-03', '2024-05-04'].includes(item.t_date)).some(t => t))
+        } else {
+          subject = "统计失败";
+
+          replyStr = `response.body.status: ${response.body.status}`;
         }
-       }else {
-      
+      } else {
+        subject = "统计失败";
 
-        subject = '统计失败'
-
-            replyStr = `response.statusCode: ${response.statusCode}`
-       }
-
-       nodemailer.createTestAccount((err,account)=>{
-        let transporter = nodemailer.createTransport(config.em);
-         // 配置内容
-         let mailOptions = {
+        replyStr = `response.statusCode: ${response.statusCode}`;
+      }
+      if (shouldEmail) {
+        nodemailer.createTestAccount((err, account) => {
+          let transporter = nodemailer.createTransport(config.em);
+          // 配置内容
+          let mailOptions = {
             from: config.em.auth.user, // 发件人地址
             to: `${config.forUser}`, //收件人
             subject: subject, // 主题
-            html: replyStr + '<br>' + '网站地址：<a  href="https://pw.hbww.org.cn/personal/index">https://pw.hbww.org.cn/personal/index </a> '
-        };
-        // 发送
-        transporter.sendMail(mailOptions,(error,info)=>{
-            if(error){
-                console.log('发送失败');
-            }else {
-                console.log('发送成功');
+            html:
+              replyStr +
+              "<br>" +
+              '网站地址：<a  href="https://pw.hbww.org.cn/personal/index">https://pw.hbww.org.cn/personal/index </a> ',
+          };
+          // 发送
+          transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+              console.log("发送失败");
+            } else {
+              console.log("发送成功");
             }
-        })
-    })
-
-    })
-
-   
-   
+          });
+        });
+      }
+    }
+  );
 });
